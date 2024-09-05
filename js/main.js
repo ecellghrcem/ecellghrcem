121cb26f9ba5057448f4c6e029210da819f3d1d8
ShubhamSahu <shubhamsahu60582@gmail.com>
2024-09-05 21:01:51 +0530

    structure fixing

--- js/main.js
+++ js/main.js
@@ -107,3 +107,10 @@
     
 })(jQuery);
 
+var item = jQuery('.extended-parallax')
+    if (item.length !== 0 ) {
+        item.each( function() {
+            jQuery(this).paroller();
+        })
+    }
+
