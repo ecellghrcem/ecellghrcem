afbf9b0c6e1a10202dfaa28690912afb617a9df3
ShubhamSahu <shubhamsahu60582@gmail.com>
2024-09-10 22:45:08 +0530

    colour black and events

--- js/main.js
+++ js/main.js
@@ -93,16 +93,16 @@
     });
 
 
-    // Portfolio isotope and filter
-    var portfolioIsotope = $('.portfolio-container').isotope({
-        itemSelector: '.portfolio-item',
+    // events isotope and filter
+    var eventsIsotope = $('.events-container').isotope({
+        itemSelector: '.events-item',
         layoutMode: 'fitRows'
     });
-    $('#portfolio-flters li').on('click', function () {
-        $("#portfolio-flters li").removeClass('active');
+    $('#events-flters li').on('click', function () {
+        $("#events-flters li").removeClass('active');
         $(this).addClass('active');
 
-        portfolioIsotope.isotope({filter: $(this).data('filter')});
+        eventsIsotope.isotope({filter: $(this).data('filter')});
     });
     
 })(jQuery);
