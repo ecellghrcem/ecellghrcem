c26204b93a6cae9a27faf58cd44cef56747d5039
ShubhamSahu <shubhamsahu60582@gmail.com>
2024-11-16 14:35:35 +0530

    1st blog updated

--- js/main.js
+++ js/main.js
@@ -113,4 +113,24 @@ var item = jQuery('.extended-parallax')
             jQuery(this).paroller();
         })
     }
-
+// scroll on blog
+document.addEventListener('scroll', function() {
+    const article = document.querySelector('.blog-article');
+    const progressBar = document.querySelector('.nav__scroll-bar');
+  
+    const articleRect = article.getBoundingClientRect();
+    const articleTop = articleRect.top;
+    const articleHeight = articleRect.height;
+  
+    const windowHeight = window.innerHeight;
+    const totalScroll = articleHeight - windowHeight;
+  
+    if (articleTop < 0 && Math.abs(articleTop) <= totalScroll) {
+      const progress = (Math.abs(articleTop) / totalScroll) * 100;
+      progressBar.style.width = `${progress}%`;
+    } else if (Math.abs(articleTop) > totalScroll) {
+      progressBar.style.width = '100%';
+    } else {
+      progressBar.style.width = '0%';
+    }
+  });
